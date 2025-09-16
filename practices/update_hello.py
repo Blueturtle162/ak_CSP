aa33ef1366a7567a8d1c15501ffb654ef9004071
Blueturtle162 <annalise.kinsey@ucas-edu.net>
2025-09-16 13:21:01 -0600

    Updated hello world code

---
+++ practices/update_hello.py
@@ -0,0 +1,11 @@
+#AK 6th Update Hello World
+
+def say_hello_world (name):
+    print ("Hello "+ name)
+    
+
+say_hello_world ("Amy")
+say_hello_world ("Josh")
+say_hello_world ("Mia")
+say_hello_world ("Kathy")
+say_hello_world ("James")

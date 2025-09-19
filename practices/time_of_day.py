79b0a2832c770b5ffe7d77769f38a54a39a7eb31
Blueturtle162 <annalise.kinsey@ucas-edu.net>
2025-09-19 13:31:36 -0600

    worked on time of day

---
+++ practices/time_of_day.py
@@ -0,0 +1,13 @@
+#AK  6th Time of Day
+
+import datetime
+
+current = datetime.datetime.now()
+hour = current.hour
+
+if hour < 12:
+    print("Good Morning.")
+elif hour < 17:
+    print("Good Afternoon.")
+else:
+    print ("Good Evening.")

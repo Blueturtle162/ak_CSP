d713fffb4747e875d83a4292c507958acac8d212
Blueturtle162 <annalise.kinsey@ucas-edu.net>
2025-10-04 01:28:26 +0000

    worked on first program

---
+++ c_programming/Notes/Notes/Practices/first_program.c
@@ -0,0 +1,11 @@
+//AK 6th First Program
+#include <stdio.h>
+
+int main(void){
+    char name[10];
+    printf("What is your first name: ");
+    scanf("%s",&name);
+    printf("Hello %s",name);
+
+    return 0;
+}

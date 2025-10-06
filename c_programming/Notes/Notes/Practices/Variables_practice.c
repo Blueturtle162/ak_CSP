d8a4f85f9c825f6065cce69852cea57f421f6723
Blueturtle162 <annalise.kinsey@ucas-edu.net>
2025-10-06 13:42:35 -0600

    worked on varibles practice

---
+++ c_programming/Notes/Notes/Practices/Variables_practice.c
@@ -0,0 +1,36 @@
+// Ak 6th Variables practice
+#include <stdio.h>
+//your name
+//a number between 1 and 10
+//a GPA
+//what you had for breakfast 
+//your favorite color
+//the school name
+//the year
+//your eye color
+//your age
+//your favorite subject in school
+
+int main (void){
+char name []= {"Annalise"}; 
+int number = 5;
+float gpa = 4.00;
+char breakfast []={"ground chicken"};
+char color [] = {"Blue"};
+char school [] = {"UCAS"};
+int year = 2025;
+char eye_color [] = {"hazel"};
+int age = 15;
+char subject [] ={"chemistry"}
+
+printf("%s",name);
+printf("%d",number);
+printf("%f",gpa);
+printf("%s",name);
+printf("%s",name);
+printf("%s",name);
+printf("%d",number);
+printf("%s",name);
+printf("%d",number);
+printf("%s",name);
+}

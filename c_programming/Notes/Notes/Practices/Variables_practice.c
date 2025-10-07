664597b750dd80b1d247a9ebb9cb1002115b6e7a
Blueturtle162 <annalise.kinsey@ucas-edu.net>
2025-10-07 01:37:03 +0000

    deleted curly braces

--- c_programming/Notes/Notes/Practices/Variables_practice.c
+++ c_programming/Notes/Notes/Practices/Variables_practice.c
@@ -2,25 +2,25 @@
 #include <stdio.h>
 
 int main (void){
-char name []= {"Annalise"}; 
+char name []= "Annalise"; 
 int number = 5;
 float gpa = 4.00;
-char breakfast []={"ground chicken"};
-char color [] = {"Blue"};
-char school [] = {"UCAS"};
+char breakfast []="ground chicken";
+char color [] = "Blue";
+char school [] = "UCAS";
 int year = 2025;
-char eye_color [] = {"hazel"};
+char eye_color [] = "hazel";
 int age = 15;
-char subject [] ={"chemistry"};
+char subject [] ="chemistry";
 
-printf("%s",name);
-printf("%d",number);
-printf("%f",gpa);
-printf("%s",breakfast);
-printf("%s",color);
-printf("%s",school);
-printf("%d",year);
-printf("%s",eye_color);
-printf("%d",age);
-printf("%s",subject);
+printf("%s/n",name);
+printf("%d/n",number);
+printf("%f/n",gpa);
+printf("%s/n",breakfast);
+printf("%s/n",color);
+printf("%s/n",school);
+printf("%d/n",year);
+printf("%s/n",eye_color);
+printf("%d/n",age);
+printf("%s/n",subject);
 }

c91abe85e6aa5bfed02d290c0f74089142a3fc9d
Blueturtle162 <annalise.kinsey@ucas-edu.net>
2025-10-07 01:41:52 +0000

    fixed problems

--- c_programming/Notes/Notes/Practices/Variables_practice.c
+++ c_programming/Notes/Notes/Practices/Variables_practice.c
@@ -1,7 +1,7 @@
 // Ak 6th Variables practice
 #include <stdio.h>
 
-int main (void){
+int main(void) {
 char name []= "Annalise"; 
 int number = 5;
 float gpa = 4.00;
@@ -13,14 +13,14 @@ char eye_color [] = "hazel";
 int age = 15;
 char subject [] ="chemistry";
 
-printf("%s/n",name);
-printf("%d/n",number);
-printf("%f/n",gpa);
-printf("%s/n",breakfast);
-printf("%s/n",color);
-printf("%s/n",school);
-printf("%d/n",year);
-printf("%s/n",eye_color);
-printf("%d/n",age);
-printf("%s/n",subject);
+printf("%s\n",name);
+printf("%d\n",number);
+printf("%f\n",gpa);
+printf("%s\n",breakfast);
+printf("%s\n",color);
+printf("%s\n",school);
+printf("%d\n",year);
+printf("%s\n",eye_color);
+printf("%d\n",age);
+printf("%s\n",subject);
 }

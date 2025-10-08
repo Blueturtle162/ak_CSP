860b45b1ad7e8e3495c36d75199f91ced21fde7f
Blueturtle162 <annalise.kinsey@ucas-edu.net>
2025-10-08 13:21:41 -0600

    worked on expressions practice

---
+++ c_programming/Practices/Equations_practice.c
@@ -0,0 +1,25 @@
+// AK 6th equations practice
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+    int equation_1 = 7-24/8*4+6;
+    int equation_2 = 18/3-7+2*5;
+    int equation_3 = 6*4/12+72/8-9;
+    int equation_4 = (17-6/2)+4*3;
+    int equation_5 = -2*(1*4-2/2)+(6+2-3);
+    int equation_6 = -1*((3-4*7)/5)-2*24/6;
+    int equation_7 = (3*pow(5,2)/15)-(5-pow(2,2));
+    int equation_8 = (pow(1,4)*pow(2,2)+pow(3,3))-pow(2,5)/4;
+    int equation_9 = pow((22/2-2*5),2)+pow((4-6/6),2);
+printf ("7-24/8x4+6= %d\n",equation_1);
+printf ("18/3-7+2x5= %d\n",equation_2);
+printf ("6x4/12+72/8-9= %d\n",equation_3);
+printf ("(17-6/2)+4x3= %d\n",equation_4);
+printf ("-2(1x4-2/2)+(6+2-3)= %d\n",equation_5);
+printf ("-1x[(3-4x7)/5]-2x24/6= %d\n",equation_6);
+printf ("(3x5^2/15)-(5-2^2)= %d\n",equation_7);
+printf ("(1^4x2^2+3^3)-2^5/4= %d\n",equation_8);
+printf ("(22/2-2x5)^2+(4-6/6)^2= %d\n",equation_9);
+}

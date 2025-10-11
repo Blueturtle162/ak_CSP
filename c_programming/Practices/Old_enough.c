10adc46768250ff13313696b7dd9746558b85234
Blueturtle162 <annalise.kinsey@ucas-edu.net>
2025-10-11 00:53:07 +0000

    deleted comments

--- c_programming/Practices/Old_enough.c
+++ c_programming/Practices/Old_enough.c
@@ -1,18 +1,4 @@
 //AK 6th Old Enough
-/*
-age = int(input("What is your age?"))
-
-if age == 15:
-    print ("You can get your learners permit.")
-elif age == 16 or age ==17:
-    print("You are old enough to drive.")
-elif age < 4:
-    print ("You are not old enogh to drive, vote, go to school, or get you learners permit.")
-elif age < 18 and age >= 4:
-    print ("You are old enough to go to school.")
-else:
-    print ("You are old enogh to vote.")
-*/
 
 #include <stdio.h>
 

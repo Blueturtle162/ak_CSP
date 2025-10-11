2b37490cc05f5a6d55de1971a02239f7e4ab5168
Blueturtle162 <annalise.kinsey@ucas-edu.net>
2025-10-11 00:52:25 +0000

    finished old enough

--- c_programming/Practices/Old_enough.c
+++ c_programming/Practices/Old_enough.c
@@ -18,12 +18,18 @@ else:
 
 int main(void){
     int age;
-    printf("How old are you?");
+    printf("How old are you?\n");
     scanf("%d",&age);
 
     if(age == 15){
-        printf("You can get your learners permit.");
+        printf("You can get your learners permit.\n");
     }else if(age == 16 || age == 17){
-        printf("You are old enough to drive.");
+        printf("You are old enough to drive.\n");
+    }else if(age < 4){
+        printf("You are not old enogh to drive, vote, go to school, or get you learners permit.\n");
+    }else if (age < 18 && age >= 4){
+        printf("You are old enough to go to school.\n");
+    }else{
+        printf("You are old enogh to vote.\n");
     }
 }
